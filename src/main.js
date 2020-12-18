f7f3af39963783999790865f1d0e5c894db21a34
Eira Mazon <eira.b.mazon@gmail.com>
2020-12-18 09:51:50 -0600

    ya podemos acceder a los cambios

--- src/main.js
+++ src/main.js
@@ -3,4 +3,16 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+var poks = data;
+ var pokemons = poks.pokemon; 
+
+ for (let i = 0; i < pokemons.length; i++){
+     let name= (pokemons[i].name);
+     document.getElementById("pokemonss").innerHTML += name;
+ }
+
+
+
+//for (var i = 0; i < pokemones.length; i++){
+//    console.log(pokemones.name[i]);
+//}
