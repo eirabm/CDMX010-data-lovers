9ac7d4b276c065320ca7fbf9f858295dc6c1d562
Eira Mazon <eira.b.mazon@gmail.com>
2020-12-21 13:00:20 -0600

    ya filtra az en consola

--- src/main.js
+++ src/main.js
@@ -4,20 +4,29 @@ import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 var poks = data;
- var pokemons = poks.pokemon; 
+var pokemons = poks.pokemon; 
+var evoluciones = pokemons.evolution;
+
+console.log(evoluciones);
+
+var arrayNombres = [];
 
 
  for (let i = 0; i < pokemons.length; i++){
     
-    let name= (pokemons[i].name);
-    let imagen= (pokemons[i].img);
-    let candys= (pokemons[i].candy-cost);
+    let name= pokemons[i].name;
+    let imagen= pokemons[i].img;
 
     document.getElementById("pokemonss").innerHTML += `
-        <div class = "cadaPokemon"> 
+        <li class= "cadaPokemon"> 
         <img src="${imagen}">
         <h2> ${name} </h2>
-        <p> ${candys} </p>
-        </div>
+        </li>
     `
+
+   arrayNombres.push(name);
 }
+
+arrayNombres.sort();
+console.log("hola", arrayNombres);
+
