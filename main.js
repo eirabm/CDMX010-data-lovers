b0238babb26fa8ca94b817b71a0bdd1cbcad1e97
Eira Mazon <eira.b.mazon@gmail.com>
2020-12-30 22:03:50 -0600

    avances de formato para pokedex

--- main.js
+++ main.js
@@ -3,30 +3,62 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-var poks = data;
-var pokemons = poks.pokemon; 
-var evoluciones = pokemons.evolution;
+const poks = data;
+const pokemons = poks.pokemon; 
 
-console.log(evoluciones);
+const colors = {
+	fire: '#FDDFDF',
+	grass: '#DEFDE0',
+	electric: '#FCF7DE',
+	water: '#DEF3FD',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: ' #98E690',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+   normal: '#F5F5F5',
+   ice: '#B6EECF',
+   steel: '#EDEAE4',
+   dark: '#8A8985',
+   ghost: '#E0BBE4'
+};
 
-var arrayNombres = [];
+const mainTypes = Object.keys(colors);
+
+
+const nombres = [];
+const imagenes = [];
 
 
  for (let i = 0; i < pokemons.length; i++){
     
-    let name= pokemons[i].name;
-    let imagen= pokemons[i].img;
+    let name = pokemons[i].name[0].toUpperCase()+pokemons[i].name.slice(1);
+    let pokeTypes = pokemons[i].type[0];
+    let imagen = pokemons[i].img;
+    let number = pokemons[i].num;
 
     document.getElementById("pokemonss").innerHTML += `
-        <li class= "cadaPokemon"> 
-        <img src="${imagen}">
-        <h2> ${name} </h2>
-        </li>
+    <class= "pokemonCard"> 
+    <div class="imgContainer">
+    <img src="${imagen}"/> </div>
+    <div class="info">
+    <span class= "number"> ${number} </span>
+    <h3 class"name">${name} </h3>
+    <small class="type"> Tipo: <span>${pokeTypes} </span></small>
+    </div>
     `
 
-   arrayNombres.push(name);
+   nombres.push(name);
+   imagenes.push(imagen);
 }
 
+
+
+
 arrayNombres.sort();
-console.log("hola", arrayNombres);
+console.log(arrayNombres);
 
