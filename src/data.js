4e69709dff9530fa68e82258196e473a1f0e2680
Alejandra García <agarcia_pacheco@hotmail.com>
2021-01-05 11:26:22 -0600

    Filtrado en orden alfabetico

--- src/data.js
+++ src/data.js
@@ -7,3 +7,11 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
+
+// let buttonSearchAlphabet = document.getElementById("search");
+
+// buttonSearchAlphabet.onclick = function(){
+//   let alphabet = names.sort();
+//   document.getElementById("pokemonss").innerHTML = alphabet;
+// };
+
