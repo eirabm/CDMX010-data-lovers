058bfe21a7a6be78d7593186ea4221ff65fe534e
Eira Mazon <eira.b.mazon@gmail.com>
2021-01-07 10:31:37 -0600

    cambios OH

--- data.js
+++ data.js
@@ -14,4 +14,22 @@ export const anotherExample = () => {
 //   let alphabet = names.sort();
 //   document.getElementById("pokemonss").innerHTML = alphabet;
 // };
+let algoalpokemon=(pokemon)=>{
+  let estudiante=`Eira y ale ya usen los pokemones ${pokemon}`
+  return estudiante
+}
 
+export let  getpokemoncito=(filterby,orderby)=>{
+  if(filterby){
+    filterby.map(pokemon=>{
+      console.error(algoalpokemon(pokemon))
+    })
+//los poquemones filtrados
+
+return filterby.length
+  }else{
+//los poquemones sin filtro
+return "ale"
+  }
+
+}
