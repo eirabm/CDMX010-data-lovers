30d536b2c89b42e4104e37dad97db23873d76dd8
Alejandra García <agarcia_pacheco@hotmail.com>
2021-01-18 16:54:04 -0600

    Responsive 1 listo

--- main.js
+++ main.js
@@ -9,27 +9,6 @@ const pokemons = poks.pokemon;
 let typesToFilter = [];
 //Aqui guardamos lod dulces, cuando el input esta checked
 let candyToFilter = [];
-//Definimos una constante con un arreglo de colores.
-const colors = {
-	fire: '#FDDFDF',
-	grass: '#b4ecb4',
-	electric: '#fcfc5c',
-	water: '#BED2F8',
-	ground: '#CEB493',
-	rock: '#d5d5d4',
-	fairy: '#F6D7DE',
-	poison: '#98d7a5',
-	bug: ' #98E690',
-	dragon: '#97b3e6',
-	psychic: '#eaeda1',
-	flying: '#F5F5F5',
-	fighting: '#E6E0D4',
-   normal: '#F5F5F5',
-   ice: '#B6EECF',
-   steel: '#EDEAE4',
-   dark: '#8A8985',
-   ghost: '#E0BBE4'
-};
 
 /*TARJETAS DE POKEMONES EN LA PANTALLA PRINCIPAL*/
 
@@ -39,56 +18,7 @@ const colors = {
 document.getElementById("pokemonss").innerHTML = `
 ${pokemons.map(pokeCard).join('')}
 `
-/*ESTILO DE LAS TARJETAS PRINCIPALES  DE LOS POKEMONES */
-
-//Se define una función, indicando el siseño que contendrá cada tarjeta Pokemón de la pantalla principal, usando un "literal Template"
-//Nota 1. "toUpperCase" esta indicando que las letra sea en mayuscula y con "slice" lo cortamos para que las siguientes se sigan viendo en minusculas.
-//Nota 2. "span" se utiliza para utilizar/enfatizar unicamente en una parte del texto y no todo un bloque como lo hace "div"
-//Nota 3. "small" se utiliza para hacer pequeño una parte del texto.
-
-function pokeCard(pokemones){
-   return `
-   <div class="pokemonCard" id= "${pokemones.num}" style="background-color:${colors[pokemones.type[0]]}; cursor: pointer" onclick= "getPokemon()">
-   <div class="imgContainer">
-   <img src="${pokemones.img}"/> </div>
-   <div class="info">
-   <span class= "number"> # ${pokemones.num} </span>
-   <h3 class = "name">${pokemones.name[0].toUpperCase()+pokemones.name.slice(1)} </h3>
-   <small class="type"> Tipo: <span>${pokemones.type[0]} </span></small>
-   </div>
-   </div>`
-};
-
-
-/*ESTILO DE LAS TARJETAS INFORMATIVAS  DE LOS POKEMONES */
-
-function pokeIndividualInfo (pokemones){
-	return `
-	<div class="individualPokeCard">
-	<div class="imgContainter">
-	<img class="pokemon" src="${pokemones.img}"/>
-	</div> 
-	<div class="cardText"> 
-	   <p class="pokename" style="background-color:${colors[pokemones.type[0]]}"> ${[pokemones.name[0].toUpperCase()+pokemones.name.slice(1)]} </p>
-	   <p class="information"> Tipo: ${pokemones.type[0]} <br>
-	   Lugar de aparición: ${pokemones.generation['name']} <br>
-	   <p class = "about">${pokemones.about}</p>
-	</div>
-	<div class="extra">
-	<div class="info" style="background-color:${colors[pokemones.type[0]]}">
-	 <div class="stats">         
-	  <h5 class = "height"> Altura: <br> ${pokemones.size['height']} </h5> 
-	  <span class="linee"></span>      
-	  <h5 class = "weight"> Peso: <br> ${pokemones.size['weight']} </h5>     
-	  <span class="linee"></span> 
-	  <h5 class = "attack"> Estadísticas <br> de ataque: <br> ${pokemones.stats['base-attack']} </h5>  
-	  <span class="linee"></span>       
-	  <h5 class = "defense"> Estadísticas <br> de defensa: <br> ${pokemones.stats['base-defense']} </h5>  
-	  <span class="linee></span>     
-	</div>   
-	</div>   
-	`
- }    
+  
 
 /* VER LA INFORMACIÓN GENERAL DE CADA POKEMÓN DE FORMA INDIVIDUAL*/
 
@@ -114,7 +44,7 @@ function getAPokemon(e) {
 		document.getElementById("pokemonss").innerHTML = `
 		${getThisPokemon.map(pokeIndividualInfo).join('')}
 		`
-	   }   
+	}   
 }
 
 /********************************************************************************** */
@@ -151,32 +81,19 @@ searchFilters.onclick = function(){
    //Se definen las variables para filtrar la información en orden alfabético.
    let arrangeAZ = document.getElementById("az").checked;
    let arrangeZA = document.getElementById("za").checked;
-   
-   //Filtro por tipo de Pokemón
-   //Utilizamos un operador ternario para definir las condiciones = Si el input de "bug" se encuentra checked, entonces se va a empujar ese insecto a la variable vacia "typesToFilter" que definimos al principio, si no no va a realizar nada.
-   document.getElementById("bug").checked ? typesToFilter.push('bug') : '';
-	document.getElementById("dark").checked ? typesToFilter.push('dark') : '';
-	document.getElementById("dragon").checked ? typesToFilter.push('dragon') : '';
-	document.getElementById("electric").checked ? typesToFilter.push('electric') : '';
-	document.getElementById("fairy").checked ? typesToFilter.push('fairy') : '';
-	document.getElementById("fighting").checked ? typesToFilter.push('fighting') : '';
-	document.getElementById("fire").checked ? typesToFilter.push('fire') : '';
-	document.getElementById("ghost").checked ? typesToFilter.push('ghost') : '';
-	document.getElementById("grass").checked ? typesToFilter.push('grass') : '';	
-	document.getElementById("ground").checked ? typesToFilter.push('ground') : '';
-	document.getElementById("ice").checked ? typesToFilter.push('ice') : '';
-	document.getElementById("normal").checked ? typesToFilter.push('normal') : '';
-	document.getElementById("poison").checked ? typesToFilter.push('poison') : '';
-	document.getElementById("psychic").checked ? typesToFilter.push('psychic') : '';
-	document.getElementById("rock").checked ? typesToFilter.push('rock') : '';
-	document.getElementById("steel").checked ? typesToFilter.push('steel') : '';
-	document.getElementById("water").checked ? typesToFilter.push('water') : '';
 
-	document.getElementById("twelveCandy").checked ? CandyToFilter.push('12') : '';
-	document.getElementById("twentyFiveCandy").checked ? CandyToFilter.push('25') : '';
-    document.getElementById("fiftyCandy").checked ? CandyToFilter.push('50') : '';
-    document.getElementById("aHundredCandy").checked ? CandyToFilter.push('100') : '';
-    document.getElementById("fourhundredCandy").checked ? CandyToFilter.push('400') : '';
+   let typeCheckboxes = document.getElementsByName("tFilter")
+   for (let i=0; i<typeCheckboxes.length; i++) {
+		if (typeCheckboxes[i].checked){
+        typesToFilter.push(typeCheckboxes[i].id)}}
+	
+	let candyCheckboxes = document.getElementsByName("cFilter")
+	for (let i=0; i<candyCheckboxes.length; i++) {
+		if (candyCheckboxes[i].checked){
+		CandyToFilter.push(candyCheckboxes[i].id)}}
+
+   
+  
 
 	if (arrangeAZ == true){
 
@@ -201,31 +118,32 @@ searchFilters.onclick = function(){
 		document.getElementById("pokemonss").innerHTML = `
 		${sortedPokemons.map(pokeCard).join('')}
 		` 
-	 }
+	}
 
    /*FILTRAR POKEMONES SEGÚN SU TIPO* ************************************/   
    // Los 3 puntos indican que estamos obteniendo los Pokemones que se guardaron en la constante "typesToFilter" y de todos los pokemones, se hace un "new set" del arreglo que corresponda a los filtros según el tipo que seleccionamos
    // Nota 1. Con "new" estamos indicando que vamos a crear un nuevo arreglo de los Pokemones ya filtrados en "typesTofIlter"
    // Nota 2. Con "set" nos aseguramos que no se repitan Pokemones, si es que se presiona 2 veces el botón buscar.
+   /*eslint no-undef: "error"*/
    let uniqueTypeFilters = [...new Set(typesToFilter)];
 
    /*FILTRAR POR EL NÚMERO DE CARAMELOS QUE NECESITA CADA POKEMON PARA EVOLUCIONAR */	
-	let uniqueCandyFilters = [...new Set(CandyToFilter)];
+	let uniqueCandyFilters = [...new Set(candyToFilter)];
 
    // Aqui estamos imprimiendo la función de getpokemoncito(que se encuentra en data), en relación a "uniquefilters (let definida con los pokemons)", "pokemons (const de los pokemones)" y "pokecard (diseño de las tarjetas)"
-   console.log (getpokemoncito(uniqueTypeFilters, uniqueCandyFilters, pokemons, pokeCard));
+   //console.log (getpokemoncito(uniqueTypeFilters, uniqueCandyFilters, pokemons, pokeCard));
 
       //Definimos una variable con la información ya filtrada totalmente de los pokemones.
       let filteredMappedPokemons = getpokemoncito(uniqueTypeFilters, uniqueCandyFilters, pokemons, pokeCard)
 
-       console.log("hola", filteredMappedPokemons)
+       //console.log("hola", filteredMappedPokemons)
 
       // Si no existen pokemones filtradsos por tipo que correspondan al numero de caramelos seleccionados, se verá el mensaje "No hay pokemones"
-	    if(filteredMappedPokemons.length == 0){
+	if(filteredMappedPokemons.length == 0){
      
       document.getElementById("pokemonss").innerHTML = `<h1>No hay ningún Pokemon disponible!</h1>`
         // Si existen pokemones filtradsos por tipo que correspondan al numero de caramelos seleccionados, se desplegarán en la pantalla.
-	    }else{
+	}else{
 		document.getElementById("pokemonss").innerHTML = filteredMappedPokemons
        }   
 }
