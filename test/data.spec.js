f2c9947762a3ac79be1ad61723a0d0e8a880d5b7
Alejandra García <agarcia_pacheco@hotmail.com>
2021-01-22 12:37:50 -0600

    Readme listo

--- test/data.spec.js
+++ test/data.spec.js
@@ -28,15 +28,28 @@ let mock= [{
         "name": "gengar",
         "candy-cost": "100"
       }]
+    }},
+    {
+      "num": "001",
+      "name": "bulbasaur",
+      "type": [
+        "grass",
+      ],
+      "evolution":{
+        "candy": "bulbasaur candy",
+        "next-evolution": [{
+        "num": "002",
+        "name": "ivysaur",
+        "candy-cost": "25",
+      }]   
     }}
-
-  ]
+]
     
 
 
 describe('esta prueba es para checar el funcionamiento de los filtros por tipo y dulces', () => {
   it('con el tipo y dulce definido el pokemon debe ser un objeto conteniendo a remoraid', () => {
-    expect(getpokemoncito(['water'], ['50'] , mock)).toEqual(expect.arrayContaining([
+    expect(getpokemoncito(['water'], ['50'], mock)).toEqual(expect.arrayContaining([
       expect.arrayContaining([
         expect.arrayContaining([
           expect.objectContaining({
@@ -48,7 +61,7 @@ describe('esta prueba es para checar el funcionamiento de los filtros por tipo y
   });
 
   it('con el tipo definido el pokemon debe ser un objeto conteniendo a haunter', () => {
-    expect(getpokemoncito(['ghost'], [] , mock)).toEqual(expect.arrayContaining([
+    expect(getpokemoncito(['ghost'], [], mock)).toEqual(expect.arrayContaining([
       expect.arrayContaining([
           expect.objectContaining({
             "name" : "haunter"
@@ -58,19 +71,44 @@ describe('esta prueba es para checar el funcionamiento de los filtros por tipo y
   });
 });
 
+  it('con el numero de dulces definidos el pokemon debe ser un objeto conteniendo a bulbasaur con 25 dulces', () => {
+    expect(getpokemoncito([], ['25'], mock)).toEqual(expect.arrayContaining([
+      expect.arrayContaining([
+        expect.objectContaining({
+          "name" : "bulbasaur"
+        })
+      ])
+    ]))
+  });
+
 describe('esta prueba checa el buscador por nombre del pokemons', () =>{
   it('busca y devuelve el pokemon con nombre Haunter', () =>{
     expect(searchName(mock, 'haunter')).toEqual(expect.arrayContaining([expect.objectContaining({"name" : "haunter"})]))
   })
-})
-
+});
 
 describe('esta prueba checa el funcionamiento de la funcion sort', () => {
   it('se organizan alfabeticamente de A-Z', () => {
-    expect(filterAZ(mock)).toEqual(expect.arrayContaining([expect.objectContaining({"name" : "remoraid"}), expect.objectContaining({"name" : "haunter"})]));
-  });
+    expect(filterAZ(mock)).toEqual(expect.arrayContaining[(
+      expect.objectContaining(
+        expect.objectContaining(
+          expect.objectContaining(
+        )
+       )
+    ))])
+  })
 
   it('se organizan alfabeticamente Z-A', () => {
     expect(filterZA(mock)).toEqual(expect.arrayContaining([expect.objectContaining({"name" : "remoraid"}), expect.objectContaining({"name" : "haunter"})]));
   });
 });
+
+[{"evolution": {"candy": "bulbasaur candy", "next-evolution":
+ [{"candy-cost": "25", "name": "ivysaur", "num": "002"}]}, 
+ "name": "bulbasaur", "num": "001", "type": ["grass"]}, 
+ {"evolution": {"candy": "gastly candy", "next-evolution":
+  [{"candy-cost": "100", "name": "gengar", "num": "094"}]}, 
+  "name": "haunter", "num": "093", "type": ["ghost", "poison"]},
+   {"evolution": {"candy": "remoraid candy", "next-evolution":
+    [{"candy-cost": "50", "name": "octillery", "num": "224"}]},
+     "name": "remoraid", "num": "223", "type": ["water"]}]
